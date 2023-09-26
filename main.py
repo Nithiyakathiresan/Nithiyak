b73427be84e60698dd872ab02bd1dbd7598f4ee9
Nithiyakathiresan <144531307+Nithiyakathiresan@users.noreply.github.com>
2023-09-26 12:10:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-class BankAccount:
+class Student:
+  def __init__(self,name,roll_number,cgpa):
+    self.name=name
+    self.roll_number=roll_number
+    self.cgpa=cgpa
 
-    def __init__(self,account_number,
-account_holder_name,initial_balance=0.0):
-           self.__account_number=account_number       
-           self.__account_holder_name=account_holder_name
-           self.__account_balance=initial_balance
+def sort_students(students_list):
+    sorted_students=sorted(students_list,               
+        key=lambda student:student.cgpa,
+                           reverse=True)
+   
+    return sorted_students
 
-    def deposit(self, amount):
-      if amount>0:
-         self.__account_balance+=amount
-         print("deposited ₹{}.new balance:₹{}". format(amount, self.__account_balance))
-      else:
-         print("invalid deposit amount.")
 
-    
-    def withdraw(self, amount):
-      if amount>0 and amount<=self.__account_balance:
-        self.__account_balance-=amount
-        print("withdraw ₹{}.New balance:₹{}.". format(amount,self.__account_balance))
+students=[                      
+     Student("Hari", "A123", 7.8),
+     Student("srikanth", "A124", 8.9), 
+     Student("savmya", "A125", 9.1),
+     Student("Mahindhar", "A126", 9.9),
+]
 
-      else:
-           print("invalid withdraw amount or insufficient balance.")
-
-    def display_balance(self):
-           print("Account balance for{}(Account # {}): ₹ {}".format(self.__account_holder_name,self.__account_number,self.__account_balance)) 
-account= BankAccount(account_number="156789", account_holder_name="Nithya", initial_balance=10000.0)
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(300.0)
-account.display_balance()
+sorted_students=sort_students(students)
+for student in sorted_students:
+    print("Name: {},Roll Number: {},CGPA:  {}".format(student.name,
+ student.roll_number,
+            student.cgpa))
